a7c01ae3586601cb065a963647d8a8925badbe4b
arvind <https://github.com/pushpendradeegwal301-arch/portfolio>
2026-04-17 20:17:07 +0530

    chore: configure vercel deployment and fix dynamic api url

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,7 @@
 const app = require('./app');
-const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to database
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
